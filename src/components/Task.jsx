06e6c7411aed98961b3c10c4694950d70a616f1f
Wenderson Santos <souowendy@gmail.com>
2023-08-30 13:42:44 -0300

    change task background to blue

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -25,7 +25,7 @@ export default function Task({ task: {id, title, state}, onArchiveTask, onPinTas
           readOnly={true}
           name='title'
           placeholder='Input title'
-          style={{background: 'red'}}
+          style={{background: 'blue'}}
         />
       </label>
 
